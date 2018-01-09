c945d2a889d73d62eb551ad33a3c065bde04e9e5
Paul H Cassidy <qawarrior@gmail.com>
2018-01-09 10:02:14 -0800

    cmd New-SemanticVersion has basic functionality

--- Cmds/New-SemanticVersion.ps1
+++ Cmds/New-SemanticVersion.ps1
@@ -1,5 +1,21 @@
 
 
+
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
 function New-SemanticVersion {
     Param (
         [Int64]$Major          = 0,
