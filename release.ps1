b3b4b3826317404861bdaba4e585c6dce71c782e
Paul H Cassidy <qawarrior@gmail.com>
2018-07-14 08:03:33 -0700

    created a publish script for releases

--- release.ps1
+++ release.ps1
@@ -1,12 +1,20 @@
+# Update FileList in manifest
 $FileList = @(
     'README.md'
     'PSSemanticVersion.psm1'
     'PSSemanticVersion.psd1'
     'LICENSE'
 )
+$FileList += Get-ChildItem "$PSScriptRoot\objs" | Resolve-Path -Relative
+$FileList += Get-ChildItem "$PSScriptRoot\en-US" | Resolve-Path -Relative
+$FileList += Get-ChildItem "$PSScriptRoot\cmds" | Resolve-Path -Relative
+Update-ModuleManifest -Path "$PSScriptRoot\PSSemanticVersion.psd1" -FileList $FileList
 
-$FileList += Get-ChildItem .\objs | Resolve-Path -Relative | Out-String
-$FileList += Get-ChildItem .\en-US | Resolve-Path -Relative | Out-String
-$FileList += Get-ChildItem .\cmds | Resolve-Path -Relative | Out-String
+# Update Version in Manifest
+$Version = Get-Content -Path "$PSScriptRoot\version.json" | ConvertFrom-Json
+$Version.Patch++
+$Version | ConvertTo-Json | Out-File -FilePath "$PSScriptRoot\version.json"
+$mv = [Version]::new($Version.Major, $Version.Minor, $Version.Patch, $Version.Build)
+Update-ModuleManifest -Path "$PSScriptRoot\PSSemanticVersion.psd1" -ModuleVersion "$mv"
 
-Publish-Module -Path . -
+Publish-Module -Path $PSScriptRoot -NuGetApiKey $APIKey
